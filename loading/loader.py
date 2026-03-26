66c4868d7792cf49ac59cf1f628c343f7c015916
srivastavaX <srivastavavisek.mail@gmail.com>
2026-03-26 10:10:29 +0545

    dump3

--- loading/loader.py
+++ loading/loader.py
@@ -6,20 +6,11 @@ import os
 from loading.db_engine import get_engine
 from loading.upsert import upsert_dataframe
 
-LOG_DIR = "logs"
-os.makedirs(LOG_DIR, exist_ok=True)
-LOG_FILE = os.path.join(LOG_DIR, "pipeline.log")
-
 logging.basicConfig(
+    filename="logs/pipeline.log",
     level=logging.INFO,
     format="%(asctime)s | %(levelname)-8s | %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[
-        logging.FileHandler(LOG_FILE),
-        logging.StreamHandler()
-    ]
 )
-
 logger = logging.getLogger(__name__)
 
 
